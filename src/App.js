2f232f98d669d826e395389db73bc01c27f1778a
Darius <dariusford15@gmail.com>
2022-01-24 21:30:59 -0500

    checking in

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+
+      <div className={'border p-4 bg-gray-400'}>
+        <h1 className={'text-center text-xl'}>QUESTION APP</h1>
+      </div>
+
+      <div className={'w-full flex'}>
+          <div className={'w-1/4 border bg-gray-600'}>
+              <ul>
+                <li className={'p-4 border'}>
+                  <h1 className={'text-xl uppercase tracking-widest'}>Category 1</h1>
+                </li>
+                <li className={'p-4 border'}>
+                  <h1 className={'text-xl uppercase'}>Category 2</h1>
+                </li>
+                <li className={'p-4 border'}>
+                  <h1 className={'text-xl uppercase'}>Category 3</h1>
+                </li>
+                <li className={'p-4 border'}>
+                  <h1 className={'text-xl uppercase'}>Category 4</h1>
+                </li>
+                <li className={'p-4 border'}>
+                  <h1 className={'text-xl uppercase'}>Category 5</h1>
+                </li>
+                
+              </ul>
+          </div>
+          <div className={'w-3/4 border bg-gray-800'}>
+
+
+          </div>
+        </div>
+
+
+      
+    
+    
+    </>
+    
   );
 }
 
